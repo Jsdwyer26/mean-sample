8a9076fae66d3dfc113f23165a455790f46aaed0
Jsdwyer26 <Jsdwyer26@gmail.com>
2016-01-21 17:17:52 -0800

    added boilerplate modules, installed node, and set-up first-level of file structure

---
+++ server.js
@@ -0,0 +1,28 @@
+/*From GA WDI tutorial: 
+https://github.com/sf-wdi-24/modules/tree/master/week-10-angular/day-04/module-01*/
+
+//I. Require express and other modules
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+
+
+
+// configure bodyParser (for receiving form data)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+
+// set view engine to hbs (handlebars)
+app.set('view engine', 'hbs');
+
+// connect to mongodb
+mongoose.connect('mongodb://localhost/mean_sample');
+
+// listen on port 3000
+app.listen(3000, function() {
+  console.log('server started');
+});
